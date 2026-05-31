456476055ea664c32a0c237f61ff8dd020f632ec
Ejidike Oluchi Amanda <ejidikeoluchiamanda@gmail.com>
2026-05-31 21:10:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 //Backend Re-design for VaultPass
-//A platform where users can create private notes and manage team access securely
-
-
-const express= require ('express');
+//A platform where users can create private notes and manage team access const express= require ('express');
 const morgan = require('morgan');
+const errorHandler = require('./src/middleware/errorHandler');
 
 require('dotenv').config();
 const app = express();
@@ -14,8 +12,11 @@ const userRoutes = require('./src/routes/user.routes');
 
 //middleware
 app.use(express.json());
-app.use(morgan('dev'));
-app.use('/api/users', userRoutes);
+app.use(morgan('dev'));app.use('/api/users', userRoutes);
+app.use('/api/moderator', require('./src/routes/moderator.routes'));
+app.use('/api/admin', require('./src/routes/admin.routes'));
+app.use(require('./src/middleware/logSuspicious'));
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
@@ -27,5 +28,5 @@ app.get('/',(req,res) =>{
 
 app.listen(PORT, ()=>{
     connectDB(); 
- console.log(`Server is running on http://localhost:${PORT}`);
+ console.log(Server is running on http://localhost:${PORT});
 });
